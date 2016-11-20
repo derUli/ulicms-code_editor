848603b0837b188a6edc3368ed280f4c6c3a707f
Ulrich Schmidt <admin@deruli.de>
2016-11-20 23:24:22 +0100

    sicherheitsprüfung

--- src/content/modules/code_editor/edit_code.php
+++ src/content/modules/code_editor/edit_code.php
@@ -1,7 +1,9 @@
 <?php
+$file = $_REQUEST ["file"];
 $acl = new ACL ();
-if ($acl->hasPermission ( "code_editor" )) {
+if ($acl->hasPermission ( "code_editor" ) and in_array ( $file, $_SESSION ["editable_code_files"] )) {
 	?>
+<h1><?php Template::escape($file);?></h1>
 Coming Soon!
 <?php
 } else {
