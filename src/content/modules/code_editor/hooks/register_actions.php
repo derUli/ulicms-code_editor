ec3d52d87e142374804561df58530c27765a8aba
Ulrich Schmidt <admin@deruli.de>
2018-02-24 13:22:21 +0100

    code_editor ist nun mit UliCMS 2018.2 kompatibel

--- src/content/modules/code_editor/hooks/register_actions.php
+++ src/content/modules/code_editor/hooks/register_actions.php
@@ -1,7 +1,6 @@
 <?php
-include_once ULICMS_ROOT . "/classes/objects/files/path.php";
 include_once ULICMS_ROOT . "/lib/formatter.php";
-include_once getModulePath ( "code_editor" ) . "controller/code_editor_controller.php";
+include_once getModulePath("code_editor") . "controller/code_editor_controller.php";
 
 global $actions;
-$actions ["edit_code"] = getModulePath ( "code_editor" ) . "edit_code.php";
+$actions["edit_code"] = getModulePath("code_editor") . "edit_code.php";
